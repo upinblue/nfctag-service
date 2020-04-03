30799c89c7be490294d50403d07313e203f40e74
domiNoeth <dominik.noeth@upinblue.com>
2020-04-03 17:07:18 +0200

    init
    init commit

---
+++ NFCTagService/Controllers/DatabaseController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+
+namespace NFCTagService.Controllers
+{
+    public class DatabaseController
+    {
+        private MySqlConnection connection;
+        public DatabaseController()
+        {
+
+            string connectionString = "";
+
+            connection = new MySqlConnection(connectionString);
+
+            connection.Open();
+        }
+
+        public MySqlConnection getConnection()
+        {
+            return connection;
+        }
+
+        public bool saveTag(string type, string serial)
+        {
+            bool success = false;
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                string sql = "INSERT INTO `factory`.`Chips` (`Type`, `SerialNumber`) VALUES ('"+ type + "', '"+ serial +"');";
+                MySqlCommand cmd = new MySqlCommand(sql, connection);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                rdr.Close();
+                cmd.Dispose();
+
+                success = true;
+            }
+            return success;
+        }
+        public bool addFlashHistory(string version)
+        {
+            bool success = false;
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                string sql = "INSERT INTO `factory`.`FlashHistory` (`Date`, `SWVersion`) VALUES (NOW(), '"+ version + "');";
+                MySqlCommand cmd = new MySqlCommand(sql, connection);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                rdr.Close();
+                cmd.Dispose();
+                success = true;
+            }
+            return success;
+        }
+    }
+}
---
+++ NFCTagService/Controllers/NFCTagService.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+using System.Net;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Text;
+using System.Net.Http;
+
+
+namespace NFCTagService.Controllers
+{
+    [ApiController]
+    [Route("v1/nfc/[controller]")]
+    [Produces("application/json")]
+    public class NFCTagService : ControllerBase
+    {
+
+        DatabaseController db;
+        public NFCTagService() { 
+            db = new DatabaseController();      
+        }
+        /* this function gets a token and check if this is valid */
+        // GET api/Authentication/authenticate
+        [HttpPost]
+        [Route("saveTag")]
+        public async System.Threading.Tasks.Task<ActionResult<IEnumerable<string>>> sendMailAsync()
+        {
+
+            string tagID = Request.Headers["TagID"];
+            string serial = Request.Headers["Serial"];
+
+            string savedURL = Request.Headers["SavedURL"];
+            string applicationKey = Request.Headers["Issuer"];
+
+            if (string.IsNullOrEmpty(tagID) || string.IsNullOrEmpty(applicationKey))
+            {
+#if DEBUG
+                Response.Headers.Add("error", "empty input");
+#endif
+                return StatusCode(400);
+            }
+
+            if (!await isApplicationEntitledAsync(applicationKey))
+            {
+#if DEBUG
+                Response.Headers.Add("error", "permission denied");
+#endif
+                return StatusCode(401);
+            }
+
+
+            if(!db.saveTag(tagID, serial))
+            {
+                return StatusCode(500);
+            }
+            return StatusCode(200);
+        }
+
+
+
+        private async System.Threading.Tasks.Task<bool> isApplicationEntitledAsync(string applicationKey)
+        {
+
+            System.Uri uri = new Uri("https://api.upinblue.com/v1/api/Entitlement/checkEntitlement");
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
+            // Create a HttpWebrequest object to the desired URL.
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(uri);
+            req.Headers["token"] = applicationKey;
+            req.Headers["entitlement"] = "1";
+            try
+            {
+                WebResponse x = await req.GetResponseAsync();
+
+                x.Close();
+                return true;
+            }
+            catch (WebException e)
+            {
+#if DEBUG
+                Response.Headers.Add("error", "checking entitlement: " + e.Message);
+#endif
+                return false;
+            }
+        }
+
+    }
+}
